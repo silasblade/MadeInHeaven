695f84813042b2a74c3cb59cb42b4579ea6a7422
Vishagna <111635138+silasblade@users.noreply.github.com>
2023-11-20 23:08:36 +0700

    Teplates

---
+++ MadeinHeavenBookStore/MadeinHeavenBookStore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MadeinHeavenBookStore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
