cbf152c867a0a30cf18d546be41690d79455c48f
Vishagna <111635138+silasblade@users.noreply.github.com>
2023-11-21 15:37:31 +0700

    Templates

---
+++ MadeinHeavenBookStore/MadeinHeavenBookStore/Controllers/PagesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MadeinHeavenBookStore.Controllers
+{
+	public class PagesController : Controller
+	{
+		public IActionResult Shop()
+		{
+			ViewData["Title"] = "Shop";
+			return View();
+		}
+
+		public IActionResult Contact()
+		{
+			ViewData["Title"] = "Contact";
+			return View();
+		}
+
+		public IActionResult ShoppingCart()
+		{
+			ViewData["Title"] = "Shopping Cart";
+			return View();
+		}
+
+		public IActionResult About()
+		{
+			ViewData["Title"] = "About";
+			return View();
+		}
+
+		public IActionResult Product()
+		{
+			ViewData["Title"] = "Product Detail";
+			return View();
+		}
+	}
+}
---
+++ REf/REf/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace REf.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
